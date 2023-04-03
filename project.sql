c35af6a4e5352c601636995b5176c754a67c42bb
MatejFrolik <matejfrolik1@seznam.cz>
2023-04-03 10:29:46 +0200

    Hlavička projektu a vypsání otázek.

---
+++ project.sql
@@ -0,0 +1,19 @@
+'''
+project.sql: První projekt do Engeto Online Datové akademie.
+author: Matěj Frolík
+email: matejfrolik1@seznam.cz
+discord: Mates F.#4204
+'''
+
+-- Výzkumné otázky
+-- 1. Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
+-- 2. Kolik je možné si koupit litrů mléka a kilogramů chleba za první 
+-- a poslední srovnatelné období v dostupných datech cen a mezd?
+-- 3. Která kategorie potravin zdražuje nejpomaleji 
+-- (je u ní nejnižší percentuální meziroční nárůst)?
+-- 4. Existuje rok, ve kterém byl meziroční nárůst 
+-- cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
+-- 5. Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, 
+-- pokud HDP vzroste výrazněji v jednom roce, 
+-- projeví se to na cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
+
